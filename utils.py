3ef2238d778f9b518b6681e0e588f0efdcb76dc3
BetaKors <leparkorsz@gmail.com>
2021-09-04 15:36:21 -0300

    added dt_as_formatted_str

--- utils.py
+++ utils.py
@@ -1,8 +1,11 @@
 import constants
 import asyncio
 
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 from discord.ext import commands
+
+
+months = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro']
 
 
 class HiddenCog(commands.Cog):
@@ -11,12 +14,31 @@ class HiddenCog(commands.Cog):
 
 
 class IFTMBotError(commands.errors.CommandError):
-    """Usado para colocar mensagens de erro customizadas."""
+    """Usado para mensagens de erro customizadas."""
     pass
 
 
 async def wait_until(dt):
     await asyncio.sleep((dt - datetime.now()).total_seconds())
+
+
+def dt_as_formatted_str(dt):
+    r = ''
+    today = datetime.today()
+    tomorrow = today + timedelta(days=1)
+
+    if dt.date() == today.date():
+        r = 'Hoje'
+
+    elif dt.date() == tomorrow.date():
+        r = 'Amanhã'
+
+    else:
+        r = f'{dt.day} de {months[dt.month - 1]}'
+
+    time = dt.strftime('%H:%M')
+
+    return f'{r} às {time}'
 
 
 def unix_timestamp_to_local_dt(timestamp):
